ef1fef1b15014b3ea307207883b76e661c5e934d
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:02:01 +0530

    first commit!

---
+++ src/com/lbrce/Areas.java
@@ -0,0 +1,19 @@
+package com.lbrce;
+
+public class Areas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Areas A=new Areas();
+		A.Circle();
+		A.Cuboid();
+
+	}
+	void Circle()
+	{
+		
+	}
+	void Cuboid()
+	 {
+		}
+}
---
+++ src/com/lbrce/RectangleArea.java
@@ -0,0 +1,14 @@
+package com.lbrce;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RectangleArea R=new RectangleArea();
+          R.Rectangle();
+	}
+       void Rectangle()
+
+       {
+    	   }
+}
